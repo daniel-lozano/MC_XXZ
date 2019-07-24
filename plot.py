60269cf2024603494fb3e0b3435224d909120472
Daniel Lozano <d.lozano343@uniandes.edu.co>
2019-07-24 15:24:37 -0400

    plot

--- plot.py
+++ plot.py
@@ -4,6 +4,8 @@ from sys import  argv
 
 size=argv[1]#input("Enter the size of the lattice=")
 FILE=np.loadtxt("Energy_magnetization_L"+size+".txt")
+FILE_O=np.loadtxt("Energy_magnetization_L"+str(30)+"_no_Jperp.txt")
+
 print(FILE.shape)
 
 T=FILE[:,0]
@@ -14,20 +16,34 @@ MA=FILE[:,4]
 MB=FILE[:,5]
 
 
+T_O=FILE_O[:,0]
+E_O=FILE_O[:,1]
+Cv_O=FILE_O[:,2]
+M_O=FILE_O[:,3]
+
+
 print(T[np.argmax(Cv)])
 plt.figure(figsize=(10,7))
 plt.subplot(311)
-plt.plot(T,E,"o-")
+plt.plot(T,E,"^-",label=size+"$\\times $"+size)
+plt.plot(T_O,E_O,"o-",label="$ 30\\times 30\ J_\perp=0 $")
 plt.ylabel("$ Energy $")
-#plt.xlabel("$ T $")
+plt.legend()
+plt.ylim(-2.5,0.0)
 plt.subplot(312)
-plt.plot(T,M,"o-")
+plt.plot(T,M,"^-",label=size+"$\\times $"+size)
+plt.plot(T_O,M_O,"o-",label="$ 30\\times 30\ J_\perp=0 $")
 plt.ylabel(" $ Magnetization $")
+plt.legend()
+
 #plt.xlabel("$ T $")
 plt.subplot(313)
-plt.plot(T,Cv,"o-",label="$ T_c= $"+str(T[np.argmax(Cv)]))
+plt.plot(T,Cv,"^-",label="$ T_c= $"+str(T[np.argmax(Cv)]))
+plt.plot(T_O,Cv_O,"o-",label="$ T_c= $"+str(T_O[np.argmax(Cv_O)]))
 plt.ylabel(" $ C $")
 plt.xlabel("$ T $")
+plt.ylim(0,2.3)
+plt.xlim(1.5,3)
 plt.legend()
 plt.show()
 
